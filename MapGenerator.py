f6b5e2b0c674856fa780484ebf27af47e7fd76f6
Eric Ndirangu <eric.ndira@gmail.com>
2020-01-06 19:44:06 +0300

    Generate the procedural generated map

---
+++ MapGenerator.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+from Map import *
+from _Generators import *
+from Colour import *
+from VectorMath import *
+
+print("Preparing... \n");
+
+
+image = Image.new("RGB", (mapSize,mapSize))
+targetGenerator = None
+
+targetGenerator = MapGen_Main();
+
+print("Processing map... \n");
+
+targetGenerator.GenerateFull();
+
+print("\n\nGeneration finished. Saving output as GeneratedMap.png.");
+
+for x in range(0, mapSize):
+    for y in range(0, mapSize):
+        image.putpixel((x,y), colorMap[x][y].GetTuple());
+
+image.save("GeneratedMap.png");
+image.show();
