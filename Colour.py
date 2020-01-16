080ee0f577ac2425bf9b7c711c77e66409456914
Eric Ndirangu <eric.ndira@gmail.com>
2020-01-16 23:00:20 +0300

    Removrd some non functional code

--- Colour.py
+++ Colour.py
@@ -1,22 +1,19 @@
 class Color:
 
-    r = 0.0;
-    g = 0.0;
-    b = 0.0;
-    a = 1.0;
-
     def __init__(self, r = 0.0, g = 0.0, b = 0.0):
         self.r = r;
         self.g = g;
         self.b = b;
         self.a = 1;
+
     def GetTuple(self):
         return (int(self.r),int(self.g),int(self.b));
+        
     def SetColor(self, r, g, b):
         self.r = r;
         self.g = g;
         self.b = b;
 
         
-paperColor = Color(212, 161, 104);
+groundColor = Color(212, 161, 104);
 waterColor = Color(48, 86, 181);
